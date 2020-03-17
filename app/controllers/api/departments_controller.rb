017a8a5b918df4959df56b9da685969281fbb1ad
Jeffrey Soriano <jeffsor17@gmail.com>
2020-03-17 14:58:09 -0600

    initial commit

---
+++ app/controllers/api/departments_controller.rb
@@ -0,0 +1,20 @@
+class Api::DepartmentsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private 
+  def department_params
+  end
+end
